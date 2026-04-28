1ef8f2151cbac9da0f43853098d84263a2fa2571
caftooz <alexandersmolin07@gmail.com>
2026-04-28 15:43:38 +0300

    Add UnitTestProject

---
+++ PR7p2/MatrixCipher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class MatrixCipher
+{
+    public static string Encrypt(string message, int row, int col)
+    {
+        return null;
+    }
+
+    public static string Decrypt(string cipher, int row, int col)
+    {
+        return null;
+    }
+}
